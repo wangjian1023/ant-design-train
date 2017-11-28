938689668cba1619e0f53b766a0f4e0a67bb19d9
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-11-28 20:35:07 +0800

    2

---
+++ trainapp/src/layouts/BasicLayout.js
@@ -0,0 +1,86 @@
+import { Layout, Menu, Icon } from 'antd';
+import React from 'react';
+import { connect } from 'dva';
+import { ContainerQuery } from 'react-container-query';
+import classNames from 'classnames';
+import { Link } from 'dva/router';
+import styles from './BasicLayout.less';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+
+// const SubMenu = Menu.SubMenu;
+const query = {
+  'screen-xs': {
+    maxWidth: 575,
+  },
+  'screen-sm': {
+    minWidth: 576,
+    maxWidth: 767,
+  },
+  'screen-md': {
+    minWidth: 768,
+    maxWidth: 991,
+  },
+  'screen-lg': {
+    minWidth: 992,
+    maxWidth: 1199,
+  },
+  'screen-xl': {
+    minWidth: 1200,
+  },
+};
+
+class BasicLayout extends React.Component {
+  render() {
+    const layout = (
+      <Layout>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <div className={styles.logo}>
+            <Link to="/">
+              <img src="https://gw.alipayobjects.com/zos/rmsportal/iwWyPinUoseUxIAeElSx.svg" alt="logo" />
+              <h1>宠物交易平台</h1>
+            </Link>
+          </div>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <SubMenu key="sub1" title={<span><Icon type="user" />宠物猫</span>}>
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="laptop" />宠物狗</span>}>
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" title={<span><Icon type="notification" />鹦鹉</span>}>
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header>header</Header>
+          <Content style={{ minHeight: 'calc(100vh - 110px)' }}>main content</Content>
+          <Footer>footer</Footer>
+        </Layout>
+      </Layout>
+  );
+    return (
+      <ContainerQuery query={query}>
+        {params => <div className={classNames(params)}>{layout}</div>}
+      </ContainerQuery>
+    );
+  }
+}
+export default connect()(BasicLayout);
